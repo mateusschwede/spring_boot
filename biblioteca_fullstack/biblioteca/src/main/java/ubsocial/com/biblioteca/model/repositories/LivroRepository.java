f5448fa509eac6f3c5725010c1d020150487bd3d
mateusschwede <mateus1908.schwede@gmail.com>
2025-06-24 11:49:24 -0300

    Spring Kotlin add

--- biblioteca_fullstack/biblioteca/src/main/java/ubsocial/com/biblioteca/model/repositories/LivroRepository.java
+++
@@ -1,6 +0,0 @@
-package ubsocial.com.biblioteca.model.repositories;
-import ubsocial.com.biblioteca.model.entity.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
